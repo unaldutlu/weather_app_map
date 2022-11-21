57e9b9e2944a73cae8774d6432d095aafc7d19e1
unaldutlu <unaldutlu@gmail.com>
2022-11-21 16:38:43 +0300

    add router and axios on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div>
+      <h1 className='App'>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
