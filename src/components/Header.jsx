e67c137d7082f267969b1b7d0939c6497d6225fd
unaldutlu <unaldutlu@gmail.com>
2022-11-22 03:40:28 +0300

    add css update on page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Header() {
   return (
     <div className='header'>
-      <h1 className='font-bold text-3xl lg:text-7xl text-orange-500 my-4'>
+      <h1 className='font-bold text-3xl lg:text-6xl 2xl:text-7xl text-orange-500 my-4'>
         Welcome
       </h1>
-      <h3 className='text-orange-500 text-xl lg:text-4xl'>
+      <h3 className='text-orange-500 text-xl lg:text-3xl 2xl:text-4xl'>
         Click for the other cities' weather.
       </h3>
     </div>
