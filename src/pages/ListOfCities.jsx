9e3cba93142d1d91b95a8a4431a5cf86fd43f405
unaldutlu <unaldutlu@gmail.com>
2022-11-24 17:00:02 +0300

    add city and css on page

--- src/pages/ListOfCities.jsx
+++ src/pages/ListOfCities.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import useTurkeyCities from "use-turkey-cities";
 
 function ListOfCities() {
+  const { cities, city, setCity, districts } = useTurkeyCities();
+
   return (
-    <div>
-      <h1>ListOfCities</h1>
+    <div className='flex justify-center items-center p-10'>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(city, districts);
+        }}
+        className='flex justify-center items-center'
+      >
+        <select
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+          value={city}
+          className='h-7 w-60 rounded-lg mr-6 px-2 outline-none show'
+        >
+          {cities.map((city) => (
+            <option key={`city-${city}`} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+        <button
+          type='submit'
+          className='bg-blue-500 text-white border-none  px-4 rounded-lg hover:bg-orange-500 h-7 show'
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
