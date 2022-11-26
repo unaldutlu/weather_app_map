d07e41743dc4439f3f25c14d9085a3b2f441f463
unaldutlu <unaldutlu@gmail.com>
2022-11-27 01:23:44 +0300

    add font family on page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Header() {
   return (
     <div className='flex flex-col justify-center items-center my-3'>
-      <h1 className='font-bold text-3xl md:text-4xl xl:text-6xl 2xl:text-6xl text-orange-500 my-4 md:mt-20 lg:mt-2'>
+      <h1 className='font-bold text-3xl md:text-4xl xl:text-6xl 2xl:text-6xl text-orange-500 my-4 md:mt-20 lg:mt-2 familyy'>
         Welcome
       </h1>
-      <h3 className='text-orange-500 text-sm md:text-xl lg:text-2xl '>
+      <h3 className='text-orange-500 text-sm md:text-lg lg:text-xl '>
         Please Click Button For Other Cities' Weather Condition
       </h3>
     </div>
